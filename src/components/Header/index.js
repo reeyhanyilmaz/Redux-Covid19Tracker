1e36c6ac11b30ecb99c345335701398ad5691796
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-15 10:59:10 +0300

    add Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <img src="assets/header.jpg" alt="headerImage" className="mt-6 w-96" />
+      <br />
+      <text>
+        <b>Global and Country Cases of Corona Virus</b>
+      </text>
+      <br />
+      <i>(select a country from below)</i>
+    </div>
+  );
+}
+
+export default Header;
