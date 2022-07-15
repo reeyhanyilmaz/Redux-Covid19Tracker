5f91e2278102ddcf29c4a183b4cf76c8949baa17
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-15 13:31:47 +0300

    add setCountryNames

--- src/redux/covidDataSlice.js
+++ src/redux/covidDataSlice.js
@@ -1,11 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+// export const fetchCovidData = createAsyncThunk("covidData/fetchCovidData", async () => {
+//         const response = await fetch("https://covid19.mathdro.id/api/confirmed");
+//         console.log('response', response);
+//         return response;
+//     });
 
 export const covidDataSlice = createSlice({
     name: "covidData",
     initialState: {
-        covidData: {},
+        covidData:"",
+        countryNames:"",
     },
-    reducers: {},
+    reducers: {
+        setCountryNames: (state, action) => {
+            state.countryNames = action.payload;           
+        }
+    },
+    extraReducers: {
+    },
 });
 
+export const { setCountryNames} = covidDataSlice.actions;
 export default covidDataSlice.reducer;
