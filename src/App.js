65e1f8b67acdcee030cc9f984e9f9a891dfae4c0
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-16 14:21:55 +0300

    ad Footer Component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Header from './components/Header';
 import SelectCountry from './components/SelectCountry';
 import Cards from './components/Cards';
 import ChartTable from './components/ChartTable';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App max-w-7xl">
+    <div className="App">
         <Header />
         <SelectCountry />
         <Cards />
         <ChartTable />
+        <Footer />
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
   return (
-    <div  className="w-screen bg-slate-200 max-w-screen-2xl">
+    <div  className="w-screen bg-slate-200 max-w-full">
       <div className="w-8/12" style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "row"}}>
          <span>
               <img src="assets/headerImg.jpg" alt="headerImage" className="my-4 mr-72 h-20 rounded-bl-full" />
